86d27ab7305ead1f355455756bc0ccc22b79f584
Kyrrin <59748840+Kochisa@users.noreply.github.com>
2025-10-07 06:23:29 +0800

    Update NhSyringe.js

--- NhSyringe.js
+++ NhSyringe.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NHentai 汉化插件
 // @namespace    https://github.com/Kochisa/NhSyringe
-// @version      1.0
+// @version      1.1
 // @description  汉化 NHentai 网站内容
 // @author       Kochisa
 // @license      MIT
